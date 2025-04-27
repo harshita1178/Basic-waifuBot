56f1ef413913c6d6e8f6078f91fd0918d361e410
harshita1178 <usekarlomujhe@gmail.com>
2025-04-28 05:12:29 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -6,7 +6,7 @@ class Config(object):
     sudo_users = "6675050163", "8156600797"
     GROUP_ID = -1002636451157
     TOKEN = "7777554185:AAG9zpq6bdDbVDIL_fMuNaVQLa8Kj92uoSg"
-    mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
+    mongo_url = "mongodb+srv://BesicWaifubot:TGDARK11798@cluster0.rg9k8ag.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     PHOTO_URL = ["https://graph.org/file/e7141e5486f22f92e35dd-1159f787abdf660245.jpg", "https://graph.org/file/4335bf26d5b8de64a5aa2-caced1ff4b83d82478.jpg"]
     SUPPORT_CHAT = "t.me/voixVerse_Group_Chat"
     UPDATE_CHAT = "https://t.me/+Gq6FEGLuwa5mMDBl"
