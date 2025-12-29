f84bab86cbb689d8d2c2fddf98e449585a2bde88
harshita1178 <usekarlomujhe@gmail.com>
2025-12-29 20:00:20 +0530

    New

--- shivu/config.py
+++ shivu/config.py
@@ -5,15 +5,15 @@ class Config(object):
     OWNER_ID = "8156600797"
     sudo_users = "6675050163", "8156600797"
     GROUP_ID = -1002636451157
-    TOKEN = "7777554185:AAG9zpq6bdDbVDIL_fMuNaVQLa8Kj92uoSg"
+    TOKEN = "8510799344:AAGdpMY8Wg1lByALSJAdxio3aMBlPqMSPyA"
     mongo_url = "mongodb+srv://BesicWaifubot:TGDARK11798@cluster0.rg9k8ag.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     PHOTO_URL = ["https://graph.org/file/e7141e5486f22f92e35dd-1159f787abdf660245.jpg", "https://graph.org/file/4335bf26d5b8de64a5aa2-caced1ff4b83d82478.jpg"]
     SUPPORT_CHAT = "t.me/voixVerse_Group_Chat"
     UPDATE_CHAT = "https://t.me/+Gq6FEGLuwa5mMDBl"
-    BOT_USERNAME = "BasicWaifuBot"
-    CHARA_CHANNEL_ID = "-1002527180063"
-    api_id = 28159105
-    api_hash = "a0936ddf210a7e091e19947c7dc70c91"
+    BOT_USERNAME = "bhang_bhosda_bot"
+    CHARA_CHANNEL_ID = "-1003678272460"
+    api_id = 21596973
+    api_hash = "ca284e55fe7dd01ae2fc55e9a7f1f8d6"
 
     
 class Production(Config):
